3f0670db405c8449086d903305ae2e5da3ff503f
Danesh M <daneshm90@gmail.com>
2013-07-29 10:12:37 -0700

    NotificationManager : Rebind the notification listeners onChange
    Rebind notification listeners when list of listeners change.
    
    Change-Id: I7a14dacdf7a44a3bd71d28748a70d751507230df

--- services/java/com/android/server/NotificationManagerService.java
+++ services/java/com/android/server/NotificationManagerService.java
@@ -1328,6 +1328,10 @@ public class NotificationManagerService extends INotificationManager.Stub
             if (uri == null || ENABLED_NOTIFICATION_LISTENERS_URI.equals(uri)) {
                 rebindListenerServices();
             }
+
+            if (uri == null || ENABLED_NOTIFICATION_LISTENERS_URI.equals(uri)) {
+                rebindListenerServices();
+            }
         }
     }
 
