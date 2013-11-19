45ad67f0e3a959c3086a4fcf365cc3bd239ef882
liquid0624 <rjbirnesser@gmail.com>
2013-11-18 23:59:10 -0500

    AOKP custom system animations (1/2)
    Ported from here: http://gerrit.aokp.co/#/c/12304/
    
    Thanks to Steve Spear
    
    Change-Id: I38bc7b83edaff3c455f0dfe3ea5ede54a20ae07c
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3285,7 +3285,27 @@ public final class Settings {
          * Wether navigation bar is on landscape on the bottom or on the right
          * @hide
          */
-        public static final String NAVIGATION_BAR_CAN_MOVE = "navigation_bar_can_move";
+        public static final String UI_FORCE_OVERFLOW_BUTTON = "ui_force_overflow_button";
+
+	/**  
+         * Custom System Animations
+   	 *  
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
+                "activity_open",
+                "activity_close",
+                "task_open",  
+                "task_close",  
+                "task_to_front",
+                "task_to_back",  
+                "wallpaper_open",  
+                "wallpaper_close",  
+                "wallpaper_intra_open",  
+                "wallpaper_intra_close",  
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
         /**
          * Navigation bar height when it is on protrait
