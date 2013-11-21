78314aa69e57872f621251f0bb63efef62ec34bc
liquid0624 <rjbirnesser@gmail.com>
2013-11-20 22:05:04 -0500

    Fix keyguard glowpad disappearing targets

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -103,9 +103,9 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
 
         }
 
-        public void onTargetChange(View v, int target) {
-
-        }
+        //public void onTargetChange(View v, int target) {
+        //
+        //}
 
         public void onFinishFinalAnimation() {
 
