aef68c3485da333b707048409f2dbeaa640365ec
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-11-28 16:01:14 +0200

    Keyguard: fix statusbar lags when default widget shown.
    When statusbar became expanded KeyguardStatusView receives
    onKeyguardVisibilityChanged callback and refreshes ClockView.
    Because it receives more than one call and every time it reads
    new values from resources it cause lags in opening statusbar panel.
    
    Replace this code with BroadcastReceiver and ContentObserver to
    refresh it only in case something was changed.
    
    Change-Id: If0dbc1db059536806852661931cb2e6436f2acf4
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -23,7 +23,6 @@ import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Handler;
-import android.os.Looper;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
@@ -55,11 +54,6 @@ public class KeyguardStatusView extends GridLayout {
         public void onTimeChanged() {
             refresh();
         }
-
-        @Override
-        void onKeyguardVisibilityChanged(boolean showing) {
-            // Do nothing
-        };
 
         @Override
         public void onScreenTurnedOn() {
@@ -149,6 +143,9 @@ public class KeyguardStatusView extends GridLayout {
 
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.TIME_12_24), false, mContentObserver);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.NEXT_ALARM_FORMATTED),
+                false, mContentObserver);
     }
 
     @Override
