835a7ff07cd8344810f0719f64a9d1e305343856
Lars Greiss <kufikugel@googlemail.com>
2013-11-30 07:40:54 +0100

    Frameworks: notification drawer background (1/2)
    so after keyguard is mostly done let us start notification drawer
    and QS. First one custom background.....either color or picture
    with custom alpha and ability to set a landscape picture seperatly
    (landscape phone only)
    
    Compared to the 4.3 included version completly recoded and better
    integrated. No idea what I thought 1 year ago on the old implementation
    
    cheeers
    
    Change-Id: Id7d0f718acd05d34fab57bb250c29ed944bdcd58
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3321,6 +3321,24 @@ public final class Settings {
         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
         /**
+         * Sets the portrait background of notification drawer
+         * @hide
+         */
+        public static final String NOTIFICATION_BACKGROUND = "notification_background";
+
+        /**
+         * Sets the landscape background of notification drawer
+         * @hide
+         */
+        public static final String NOTIFICATION_BACKGROUND_LANDSCAPE = "notification_background_landscape";
+
+        /**
+         * Sets the alpha (transparency) of notification wallpaper
+         * @hide
+         */
+        public static final String NOTIFICATION_BACKGROUND_ALPHA = "notification_background_alpha";
+
+        /**
          * Navigation bar button color
          * @hide
          */
